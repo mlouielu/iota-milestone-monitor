396c614728a23f2d6b97562db1bbd30b3ed5a476
Louie Lu <git@louie.lu>
2017-12-30 17:45:01 +0800

    Change datetime format

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,7 @@ def format_datetime(value, format='medium'):
     if format == 'full':
         format="EEEE, d. MMMM y 'at' HH:mm"
     elif format == 'medium':
-        format="EE dd.MM.y HH:mm"
+        format="y/MM/dd HH:mm"
     return babel.dates.format_datetime(value, format)
 
 
