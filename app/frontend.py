b2222cfcbd88c8fca2a5035252d125c2dc7cc9cc
Louie Lu <git@louie.lu>
2017-12-30 17:03:10 +0800

    Add milestones list

--- app/frontend.py
+++ app/frontend.py
@@ -5,7 +5,7 @@
 # You can find out more about blueprints at
 # http://flask.pocoo.org/docs/blueprints/
 
-from flask import Blueprint, render_template, flash, redirect, url_for
+from flask import Blueprint, render_template, flash, redirect, url_for, request
 from flask_bootstrap import __version__ as FLASK_BOOTSTRAP_VERSION
 from flask_nav.elements import Navbar, View, Subgroup, Link, Text, Separator
 from markupsafe import escape
@@ -47,3 +47,31 @@ def index():
     mindex = get_latest_index()
     milestones = get_milestone({'mindex >': mindex - 5})
     return render_template('index.html', milestones=milestones, nodeinfo=nodeinfo)
+
+
+def get_maxp(mindex):
+    return (mindex - 243000) // 50 + 1
+
+def get_page(page, mindex):
+    max_page = get_maxp(mindex) + 1
+    start = 1 if page - 5 < 1 else page - 5
+    end = max_page if page + 5 > max_page else page + 5
+    if end - start < 10 and end != max_page:
+        end = start + 9
+    return start, end
+
+
+@frontend.route('/milestones')
+def milestones():
+    page = int(request.args.get('p', 1))
+    mindex = get_latest_index()
+    maxp = get_maxp(mindex)
+
+    if page > maxp or page < 1:
+        return "milestone page error"
+    
+    milestones = get_milestone(
+        {'mindex >': mindex - 50 * page, 'mindex <=': mindex - (50 * (page - 1))})
+    start, end = get_page(page, mindex)
+    return render_template('milestones.html', milestones=milestones,
+                           page=page, start=start, end=end, maxp=maxp)
